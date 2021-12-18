a85829074f9e52f18ab9035f399045f0210ed490
ashimmaharjan <ciferulquiorra008@gmail.com>
2021-12-18 09:43:27 +0545

    Products Added

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -42,6 +42,96 @@ const FeaturedProducts = () => {
                     </div>
                 </div>
 
+                <div className="col-span-1 h-auto relative">
+                    <div className="absolute text-xs w-auto py-1 px-3 flex items-center justify-center left-3 top-3 bg-red-400 font-sans text-white rounded-3xl">
+                        <h1>-17%</h1>
+                    </div>
+                    <img src={CardPhotos} className="w-full h-64 object-cover" alt="" />
+                    <div className="flex justify-between mt-3">
+                        <div>
+                            <h1 className="text-gray-500 text-xs">Fashion, Featured Product</h1>
+                            <h1>Yellow Men Shoes</h1>
+                            <p className="inline-flex">
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                            </p>
+                            <div className="flex items-center font-semibold">
+                                <p className="text-gray-500 line-through">Rs.5900</p>
+                                <p className="text-lg ml-3">Rs.4900</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button>
+                                <RiHeart2Line className="text-gray-400 w-5 h-5"></RiHeart2Line>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="col-span-1 h-auto relative">
+                    <div className="absolute text-xs w-auto py-1 px-3 flex items-center justify-center left-3 top-3 bg-red-400 font-sans text-white rounded-3xl">
+                        <h1>-17%</h1>
+                    </div>
+                    <img src={CardPhotos} className="w-full h-64 object-cover" alt="" />
+                    <div className="flex justify-between mt-3">
+                        <div>
+                            <h1 className="text-gray-500 text-xs">Fashion, Featured Product</h1>
+                            <h1>Yellow Men Shoes</h1>
+                            <p className="inline-flex">
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                            </p>
+                            <div className="flex items-center font-semibold">
+                                <p className="text-gray-500 line-through">Rs.5900</p>
+                                <p className="text-lg ml-3">Rs.4900</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button>
+                                <RiHeart2Line className="text-gray-400 w-5 h-5"></RiHeart2Line>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="col-span-1 h-auto relative">
+                    <div className="absolute text-xs w-auto py-1 px-3 flex items-center justify-center left-3 top-3 bg-red-400 font-sans text-white rounded-3xl">
+                        <h1>-17%</h1>
+                    </div>
+                    <img src={CardPhotos} className="w-full h-64 object-cover" alt="" />
+                    <div className="flex justify-between mt-3">
+                        <div>
+                            <h1 className="text-gray-500 text-xs">Fashion, Featured Product</h1>
+                            <h1>Yellow Men Shoes</h1>
+                            <p className="inline-flex">
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                                <TiStar className="text-gray-400 w-4 h-4"></TiStar>
+                            </p>
+                            <div className="flex items-center font-semibold">
+                                <p className="text-gray-500 line-through">Rs.5900</p>
+                                <p className="text-lg ml-3">Rs.4900</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button>
+                                <RiHeart2Line className="text-gray-400 w-5 h-5"></RiHeart2Line>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
         </div >
     );
